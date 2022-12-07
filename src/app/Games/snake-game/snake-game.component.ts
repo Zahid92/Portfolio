8572b88aac6e98a14fe90e505814923e85fe5438
Zahid92 <ansarizahid2098@gmail.com>
2022-12-07 18:09:36 +0530

    added Snake Game Component

---
+++ src/app/Games/snake-game/snake-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-snake-game',
+  templateUrl: './snake-game.component.html',
+  styleUrls: ['./snake-game.component.css']
+})
+export class SnakeGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SnakeGameComponent } from './Games/snake-game/snake-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SnakeGameComponent
   ],
   imports: [
     BrowserModule,
