f65236ee50010f14ad8b4fe56aa44bdda39c76f6
zahid92 <ansarizahid2098@gmail.com>
2025-01-05 00:38:01 +0530

    added snakes's head

--- src/app/games/models/snake.ts
+++ src/app/games/models/snake.ts
@@ -3,46 +3,50 @@ import { IStats } from "./stats";
 
 export class Snake {
     init_length: number = 5;
-    cells: Cell[];
-    direction: string ;
+    snakeCells: Cell[];
+    direction: string;
+    snakeHeadColor = "purple";
 
     constructor(canvasWidth: number) {
-        this.cells = [];
+        this.snakeCells = [];
         this.direction = 'down';
         for (var i = this.init_length - 1; i >= 0; i--) {
-            this.cells.push(new Cell({ x: canvasWidth / 20, y: i, color: "yellow" }));
+            this.snakeCells.push(new Cell({ x: canvasWidth / 20, y: i, color: "yellow" }));
         }
     }
 
     drawSnake(pen: any): void {
-        for (var i = 0; i < this.cells.length; i++) {
-            pen.fillStyle = this.cells[i].color;
+        for (var i = 0; i < this.snakeCells.length; i++) {
+            pen.fillStyle = this.snakeCells[i].color;
             pen.strokeStyle = "black";
             pen.lineWidth = 5;
-            pen.strokeRect(this.cells[i].x * 10, this.cells[i].y * 10, 10, 10);
-            pen.fillRect(this.cells[i].x * 10, this.cells[i].y * 10, 10, 10);
+            pen.strokeRect(this.snakeCells[i].x * 10, this.snakeCells[i].y * 10, 10, 10);
+            pen.fillRect(this.snakeCells[i].x * 10, this.snakeCells[i].y * 10, 10, 10);
         }
     }
 
     updateSnake(obj: IStats): void {
-        var headX = this.cells[0].x;
-        var headY = this.cells[0].y;
+        var headX = this.snakeCells[0].x;
+        var headY = this.snakeCells[0].y;
 
-        //if food strikes with snake
-        for (var i = 1; i < this.cells.length; i++) {
-            if (this.cells[i].x == headX && this.cells[i].y == headY) {
+        //detect collison with itself
+        for (var i = 1; i < this.snakeCells.length; i++) {
+            if (this.snakeCells[i].x == headX && this.snakeCells[i].y == headY) {
                 obj.gameOver = true;
+                this.snakeCells[i].color = this.snakeHeadColor;
+                return
             }
         }
 
+        //if food strikes with snake
         if (headX == obj.food.x && headY == obj.food.y) {
             obj.score++;
-            this.cells[0].color = obj.food.color;
+            this.snakeCells[0].color = obj.food.color;
             obj.food.setNextRandomCell(obj.canvasWidth, obj.canvasHeight);
         }
         else {
             //if food not taken;
-            this.cells.pop();
+            this.snakeCells.pop();
         }
 
         var nextX: number = 0;
@@ -74,14 +78,15 @@ export class Snake {
 
         }
 
-        //insert the new cell at head
-        this.cells.unshift(new Cell({ x: nextX, y: nextY, color: "yellow" }));
+        //insert the new cell at head and update previous head
+        this.snakeCells[0].color = "yellow";
+        this.snakeCells.unshift(new Cell({ x: nextX, y: nextY, color: "purple" }));
 
         //last coordinates condition
         var last_x = Math.round(obj.canvasWidth / 10);
         var last_y = Math.round(obj.canvasHeight / 10);
 
-        if (this.cells[0].y < 0 || this.cells[0].x > last_x || this.cells[0].x < 0 || this.cells[0].y > last_y) {
+        if (this.snakeCells[0].y < 0 || this.snakeCells[0].x > last_x || this.snakeCells[0].x < 0 || this.snakeCells[0].y > last_y) {
             obj.gameOver = true;
         }
     }
