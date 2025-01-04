b1aa92bc613222441a4feb326633353281d0fcf1
zahid92 <ansarizahid2098@gmail.com>
2025-01-04 23:37:52 +0530

    fontstyle added globally

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -13,7 +13,7 @@ export class ProjectsComponent implements OnInit {
     this.projects = [];
     var des = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,"
     for (var i = 0; i < 10; i++) {
-      this.projects.push(new Project(i, "project", "link", "img", des, "solo", "Me"));
+      //this.projects.push(new Project(i, "project", "link", "img", des, "solo", "Me"));
     }
   }
 
